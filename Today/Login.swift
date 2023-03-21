a0262bbea305e33da5066fa0be3cf6aafe205b9b
serifeturksever <62253012+serifeturksever@users.noreply.github.com>
2023-03-21 19:03:49 +0300

    login page is done.

--- Today/Login.swift
+++ Today/Login.swift
@@ -13,78 +13,70 @@ class Login: UIViewController {
     private let email = CustomTextField(fieldType: .email)
     private let password = CustomTextField(fieldType: .password)
     
+    private let signInBtn = CustomButton(title: "Sign In", hasBackground: true, fontSize: .med)
+    private let registerBtn = CustomButton(title: "Create Account" , fontSize: .small)
+    private let forgotPassBtn = CustomButton(title: "Forgot Password", fontSize: .small)
+    
+    private let authHeader = AuthHeaderView(title: "Sign In", subTitle: "Sign in to your account")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupUI()
-        
         
     }
     
     private func setupUI(){
         self.view.backgroundColor = .systemBackground
         
+        self.view.addSubview(authHeader)
         self.view.addSubview(email)
         self.view.addSubview(password)
+        self.view.addSubview(signInBtn)
+        self.view.addSubview(registerBtn)
+        self.view.addSubview(forgotPassBtn)
         
+        self.authHeader.translatesAutoresizingMaskIntoConstraints = false
         self.email.translatesAutoresizingMaskIntoConstraints = false
         self.password.translatesAutoresizingMaskIntoConstraints = false
+        self.signInBtn.translatesAutoresizingMaskIntoConstraints = false
+        self.registerBtn.translatesAutoresizingMaskIntoConstraints = false
+        self.forgotPassBtn.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            self.email.topAnchor.constraint(equalTo: self.view.layoutMarginsGuide.topAnchor, constant: 40),
-            self.email.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            
+            self.authHeader.topAnchor.constraint(equalTo: self.view.layoutMarginsGuide.topAnchor, constant: 40),
+            self.authHeader.heightAnchor.constraint(equalToConstant: 222),
+            self.authHeader.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.authHeader.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            
+            
+            self.email.topAnchor.constraint(equalTo: self.authHeader.bottomAnchor, constant: 20),
+            self.email.centerXAnchor.constraint(equalTo: self.authHeader.centerXAnchor),
             self.email.heightAnchor.constraint(equalToConstant: 55),
             self.email.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.85),
             
             self.password.topAnchor.constraint(equalTo: self.email.bottomAnchor, constant: 20),
             self.password.centerXAnchor.constraint(equalTo: self.email.centerXAnchor),
             self.password.heightAnchor.constraint(equalToConstant: 55),
-            self.password.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.85)
+            self.password.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.85),
+            
+            self.signInBtn.topAnchor.constraint(equalTo: self.password.bottomAnchor, constant: 20),
+            self.signInBtn.centerXAnchor.constraint(equalTo: self.password.centerXAnchor),
+            self.signInBtn.heightAnchor.constraint(equalToConstant: 55),
+            self.signInBtn.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.85),
+            
+            self.registerBtn.topAnchor.constraint(equalTo: self.signInBtn.bottomAnchor, constant: 20),
+            self.registerBtn.centerXAnchor.constraint(equalTo: self.signInBtn.centerXAnchor),
+            //self.registerBtn.heightAnchor.constraint(equalToConstant: 55),
+            self.registerBtn.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.85),
+            
+            self.forgotPassBtn.topAnchor.constraint(equalTo: self.registerBtn.bottomAnchor, constant: 15),
+            self.forgotPassBtn.centerXAnchor.constraint(equalTo: self.registerBtn.centerXAnchor),
+            //self.forgotPassBtn.heightAnchor.constraint(equalToConstant: 55),
+            self.forgotPassBtn.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.85)
             
         ])
     }
     
 }
-
-class CustomTextField: UITextField {
-    
-    enum CustomTextFieldType {
-        case username
-        case email
-        case password
-    }
-    
-    private let authFieldType: CustomTextFieldType
-    
-    init(fieldType: CustomTextFieldType) {
-        self.authFieldType = fieldType
-        super.init(frame: .zero)
-        
-        self.backgroundColor = .secondarySystemBackground
-        self.layer.cornerRadius = 10
-        
-        self.returnKeyType = .done
-        self.autocorrectionType = .no
-        self.autocapitalizationType = .none
-        
-        self.leftViewMode = .always
-        self.leftView = UIView(frame: CGRect(x:0, y:0,width: 12, height: self.frame.size.height))
-        
-        switch fieldType {
-        case .username:
-            self.placeholder = "Username"
-        case .email:
-            self.placeholder = "Email Address"
-            self.keyboardType = .emailAddress
-            self.textContentType = .emailAddress
-        case .password:
-            self.placeholder = "Password"
-            self.textContentType = .oneTimeCode
-            self.isSecureTextEntry = true
-        }
-    }
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
-
 
