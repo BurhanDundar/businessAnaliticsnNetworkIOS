7d8bf9c6a0a655c130f232676151e4c2287b449a
Burhan Dundar <burhan.dundar@ykteknoloji.com.tr>
2023-06-23 12:57:05 +0300

    .

--- Today/Controller/CompanyDetailViewController/CompanyViewController.swift
+++ Today/Controller/CompanyDetailViewController/CompanyViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class CompanyViewController: UIViewController {
     
-    var tryButton = CustomButton(title: "Burada Çalışanları Gör", hasBackground: true ,fontSize: .med)
+    var tryButton = CustomButton(title: "See All Experiencers", hasBackground: true ,fontSize: .med)
     
     var systemImageName: String!
     var company: Company?
     var isCompanyBookmarked: Bool?
+    var experienceCount: Int = 0
+    
+    var experienceCountLabel = UILabel()
     
     lazy var fetchedImageView: UIImageView = {
         let iv = UIImageView()
@@ -22,31 +25,47 @@ class CompanyViewController: UIViewController {
         iv.contentMode = .scaleAspectFill
         iv.backgroundColor = .clear
         iv.layer.borderWidth = 1
-        iv.layer.borderColor = UIColor.blue.cgColor
-        iv.layer.cornerRadius = iv.frame.size.height/2
+//        iv.layer.borderColor = UIColor.blue.cgColor
+//        iv.layer.cornerRadius = iv.frame.size.height/2
         iv.clipsToBounds = true
         return iv
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.getCompanyExperienceCount()
+        
         systemImageName = (self.isCompanyBookmarked ?? false) ? "bookmark.fill" :  "bookmark"
         let bookmarkBarButton = UIBarButtonItem(image: UIImage(systemName: systemImageName), style: .plain, target: self, action: #selector(bookmarkCompany))
         navigationItem.rightBarButtonItem = bookmarkBarButton
-                
-        view.backgroundColor = .systemBackground //.white
+        view.backgroundColor = .systemBackground
         
         let companyName = UILabel()
         companyName.text = company?.name
+        companyName.font = .boldSystemFont(ofSize: 22)
+        companyName.textColor = .systemBlue
+        companyName.textAlignment = .center
+        companyName.numberOfLines = 0
+        companyName.sizeToFit()
+        
+        experienceCountLabel.text = "\(experienceCount) Ege University Computer Engineering members have been worked in this company 👩🏼‍💻👨🏽‍💻"
+        experienceCountLabel.font = .preferredFont(forTextStyle: .body, compatibleWith: .none)
+        experienceCountLabel.textAlignment = .center
+        experienceCountLabel.numberOfLines = 0
+        experienceCountLabel.sizeToFit()
         
         fetchedImageView.translatesAutoresizingMaskIntoConstraints = false
-        tryButton.translatesAutoresizingMaskIntoConstraints = false
         companyName.translatesAutoresizingMaskIntoConstraints = false
+        tryButton.translatesAutoresizingMaskIntoConstraints = false
+        experienceCountLabel.translatesAutoresizingMaskIntoConstraints = false
+        
         
         loadFetchedImage(for: company?.image ?? "")
         
         self.view.addSubview(fetchedImageView)
         self.view.addSubview(companyName)
+        self.view.addSubview(experienceCountLabel)
         self.view.addSubview(self.tryButton)
         
         
@@ -57,10 +76,16 @@ class CompanyViewController: UIViewController {
             fetchedImageView.heightAnchor.constraint(equalToConstant: 200),
             
             companyName.centerXAnchor.constraint(equalTo: self.fetchedImageView.centerXAnchor),
-            companyName.topAnchor.constraint(equalTo: self.fetchedImageView.bottomAnchor, constant: 100),
+            companyName.topAnchor.constraint(equalTo: self.fetchedImageView.bottomAnchor, constant: 60),
+            companyName.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.85),
             
-            self.tryButton.topAnchor.constraint(equalTo: companyName.bottomAnchor, constant: 100),
-            self.tryButton.centerXAnchor.constraint(equalTo: companyName.centerXAnchor),
+            experienceCountLabel.topAnchor.constraint(equalTo: companyName.bottomAnchor, constant: 40),
+            experienceCountLabel.centerXAnchor.constraint(equalTo: companyName.centerXAnchor),
+            experienceCountLabel.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.85),
+            
+            
+            self.tryButton.topAnchor.constraint(equalTo: experienceCountLabel.bottomAnchor, constant: 100),
+            self.tryButton.centerXAnchor.constraint(equalTo: experienceCountLabel.centerXAnchor),
             self.tryButton.heightAnchor.constraint(equalToConstant: 55),
             self.tryButton.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.75)
         ])
@@ -96,4 +121,42 @@ class CompanyViewController: UIViewController {
     private func loadFetchedImage(for url: String){
         fetchedImageView.loadImage(url, "company")
     }
+    
+    private func getCompanyExperienceCount(){
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let stringURL = "\(appDelegate.APIURL)/experience/companyExperienceCount"
+            let params = [
+                "companyId": company?.id
+            ]
+        
+            guard let url = URL(string: stringURL) else { return }
+            
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.setValue("Application/json", forHTTPHeaderField: "Content-Type")
+            request.httpBody = try? JSONSerialization.data(withJSONObject: params, options: [])
+            
+            let session = URLSession.shared.dataTask(with: request) { data, response, error in
+            
+                guard let data = data else { return }
+                
+                if let error = error {
+                    print("there was an error: \(error.localizedDescription)")
+                }
+                
+                do {
+                    let decoder = JSONDecoder()
+                    let response = try decoder.decode(CompanyExperienceCountResponseModel.self, from: data)
+                    DispatchQueue.main.async {
+                        self.experienceCount = response.count
+                        self.experienceCountLabel.text = "\(self.experienceCount) Ege University Computer Engineering members have been worked in this company 👩🏼‍💻👨🏽‍💻"
+                    }
+                } catch {
+                    print("Error Occured!")
+                }
+                
+            }
+            
+            session.resume()
+        }
 }
---
+++ Today/Model/CompanyExperienceCountResponseModel.swift
@@ -0,0 +1,12 @@
+//
+//  CompanyExperienceCountResponseModel.swift
+//  Today
+//
+//  Created by Yapı Kredi Teknoloji A.Ş. on 23.06.2023.
+//
+
+import Foundation
+
+struct CompanyExperienceCountResponseModel: Codable {
+    var count: Int
+}
